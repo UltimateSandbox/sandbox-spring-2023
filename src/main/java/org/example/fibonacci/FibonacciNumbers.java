4f29ce953087710bf486be9b64a6cb8cbf924972
spyro1001 <raymondhedgecock@gmail.com>
2023-01-24 19:32:40 -0700

    fib

---
+++ src/main/java/org/example/fibonacci/FibonacciNumbers.java
@@ -0,0 +1,44 @@
+package org.example.fibonacci;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FibonacciNumbers {
+
+    private Map<Integer, Long> dictionary = new HashMap<>();
+
+    public static void main(String[] args) {
+
+        FibonacciNumbers fn = new FibonacciNumbers();
+
+        for (int x = 0; x <= 50; x++) {
+            System.out.println("fib(" + x + ") = " + (fn.fib(x)));
+        }
+    }
+
+    private long fib(int x) {
+
+        if (x == 0) {
+            return 0L;
+        }
+        if (x == 1) {
+            return 1L;
+        }
+        return memo(x - 1) + memo(x - 2);
+
+    }
+
+    private long memo(int i) {
+
+        Long value = dictionary.get(i);
+
+        if (value != null) {
+            return value;
+        }
+
+        value = fib(i);
+        dictionary.put(Integer.valueOf(i), value);
+        return value;
+    }
+
+}
