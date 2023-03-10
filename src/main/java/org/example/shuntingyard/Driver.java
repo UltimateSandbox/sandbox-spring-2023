3de31ee65e9d3dc9495a4db9eb6f66c4d8bb0e6b
spyro1001 <raymondhedgecock@gmail.com>
2023-03-09 19:38:59 -0700

    adding shunting yard

---
+++ src/main/java/org/example/shuntingyard/Driver.java
@@ -0,0 +1,9 @@
+package org.example.shuntingyard;
+
+public class Driver {
+
+    public static void main(String[] args) {
+
+        System.out.println(ShuntingYard.postFix("3+4*5"));
+    }
+}
--- src/main/java/org/example/shuntingyard/ShuntingYard.java
+++ src/main/java/org/example/shuntingyard/ShuntingYard.java
@@ -1,83 +1,110 @@
 package org.example.shuntingyard;
 
+import java.security.InvalidParameterException;
 import java.util.*;
+import java.util.stream.Collectors;
+
 public class ShuntingYard {
 
     private static String REGEX_OPERATOR = "^[+\\-*\\/]$";
+    private static String REGEX_LEFT_PARENTHESIS = "\\(";
+    private static String REGEX_NUMERIC = "^[0-9]$";
 
     public static String postFix(String inFix) {
 
-        // preprocessing
         String[] inputChars = inFix.split("");
         Queue<String> input = new LinkedList<>();
         Queue<String> output = new LinkedList<>();
         Stack<String> operatorStack = new Stack<>();
 
-        // load input queue
         Arrays.asList(inputChars).stream()
                 .forEach(c -> input.offer(c));
 
-        while(!input.isEmpty()) {
+        while (!input.isEmpty()) {
 
-            // read a token
             String token = input.poll();
 
-            if(isNumeric(token)) {
+            if (isNumeric(token)) {
                 output.offer(token);
                 continue;
             }
 
-            if(isFunction(token)) {
+            if (isFunction(token)) {
                 operatorStack.push(token);
                 continue;
             }
 
-            if(isOperator(token)) {
+            if (isOperator(token)) {
+
                 while(!operatorStack.isEmpty() &&
-                operatorStack.peek().matches(REGEX_OPERATOR) &&
-                !operatorStack.peek().matches("\\(")) {
-
+                        operatorStack.peek().matches(REGEX_OPERATOR) &&
+                        !operatorStack.peek().matches(REGEX_LEFT_PARENTHESIS)) {
+                    // TODO implement operator precedence
+                    output.offer(operatorStack.pop());
                 }
+                operatorStack.push(token);
+                continue;
             }
 
-            if(isLeftParenthesis(token)) {
+            if (isLeftParenthesis(token)) {
 
+                operatorStack.push(token);
+                continue;
             }
 
-            if(isRightParenthesis(token)) {
-                while() {
+            if (isRightParenthesis(token)) {
 
+                while(!operatorStack.isEmpty() &&
+                        !operatorStack.peek().matches(REGEX_LEFT_PARENTHESIS)) {
+                    output.offer(operatorStack.pop());
                 }
+
+                if(operatorStack.peek().matches(REGEX_LEFT_PARENTHESIS)) {
+                    operatorStack.pop();
+                    continue;
+                }
+
+                if(isFunction(operatorStack.peek())) {
+                    output.offer(operatorStack.pop());
+                }
+
             }
+
 
         }
 
-        while() {
-
+        while (!operatorStack.isEmpty()) {
+            if ("(".compareTo(operatorStack.peek()) != 0) {
+                output.offer(operatorStack.pop());
+            } else {
+                throw new InvalidParameterException("Input mismatch.");
+            }
         }
 
-        String postFix = "";
+        String postFix = output.stream()
+                .collect(Collectors.joining());
         return postFix;
     }
 
     private static boolean isRightParenthesis(String token) {
-        return false;
+        return ")".compareTo(token) == 0;
     }
 
     private static boolean isLeftParenthesis(String token) {
-        return false;
+        return "(".compareTo(token) == 0;
     }
 
     private static boolean isOperator(String token) {
-        return false;
+        return token.matches(REGEX_OPERATOR);
     }
 
     private static boolean isFunction(String token) {
-        // TODO implement this
+        // TODO implement
         return false;
     }
 
     private static boolean isNumeric(String token) {
-        return token.matches("^[0-9]$");
+        return token.matches(REGEX_NUMERIC);
     }
+
 }
