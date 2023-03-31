66ff7bc89a2cad9c8c288f816892489c4c2d2255
spyro1001 <raymondhedgecock@gmail.com>
2023-03-30 18:41:29 -0700

    adding factorial

---
+++ src/main/java/org/example/recursion/factorial/Factorial.java
@@ -0,0 +1,21 @@
+package org.example.recursion.factorial;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+
+        for (int x = 0; x <= 50; x++) {
+            System.out.println("Factorial(" + x + ") = " + factorial(x));
+        }
+    }
+
+    private static long factorial(int x) {
+
+        // base case
+        if (x == 0) {
+            return 1;
+        }
+
+        return x * factorial(x - 1);
+    }
+}
